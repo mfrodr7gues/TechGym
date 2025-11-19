789ead019341122758c0b20ca9241f5a89af1ae5
Luilu-star <luiotv2302@gmail.com>
2025-11-19 09:12:59 -0300

    ajeitando matricula e pagamento

--- src/classes/Matricula.java
+++ src/classes/Matricula.java
@@ -13,14 +13,43 @@ public class Matricula {
 
     public Matricula(int idMatricula, Date dataInicio, Date dataFim, boolean statusMatricula, Aluno aluno, Plano plano, Pagamento tipoPagamento) {
         
-    this.idMatricula = idMatricula;
-    this.dataInicio = dataInicio;
-    this.dataFim = dataFim;
-    this.statusMatricula = statusMatricula;
-    this.aluno = aluno;
-    this.plano = plano;
-    this.tipoPagamento = tipoPagamento;
-}
+        this.idMatricula = idMatricula;
+        this.dataInicio = dataInicio;
+        this.dataFim = dataFim;
+        this.statusMatricula = statusMatricula;
+        this.aluno = aluno;
+        this.plano = plano;
+        this.tipoPagamento = tipoPagamento;
+    }
 
+    private int getMatricula() {
+        return idMatricula;
+    }
 
+    private Date addMonths(Date date, int months) {
+        java.util.Calendar cal = java.util.Calendar.getInstance();
+        cal.setTime(date != null ? date : new Date());
+        cal.add(java.util.Calendar.MONTH, months);
+        return cal.getTime();
+    }
+
+    public void realizarMatricula() {
+        this.dataInicio = new Date();
+        this.dataFim = addMonths(this.dataInicio, 1);
+        this.statusMatricula = true;
+    }
+
+    public void cancelarMatricula() {
+        this.statusMatricula = false;
+        this.dataFim = new Date();
+    }
+
+    public void renovarMatricula() {
+        if (!this.statusMatricula) {
+            realizarMatricula();
+            return;
+        }
+        this.dataFim = addMonths(this.dataFim, 1);
+        this.statusMatricula = true;
+    }
 }
--- src/classes/Pagamento.java
+++ src/classes/Pagamento.java
@@ -1,5 +1,36 @@
 package classes;
+import java.util.Date;
 
 public class Pagamento {
-    private int idPagamento; 
+    private int idPagamento;
+    private Date dataPagamento;
+    private float valorPagamento;
+    private String metodoPagamento;
+    private boolean statusPagamento;
+    private int idMatricula;
+
+    public Pagamento(int idPagamento, Date dataPagamento, float valorPagamento, String metodoPagamento, boolean statusPagamento, int idMatricula) {
+        this.idPagamento = idPagamento;
+        this.dataPagamento = dataPagamento;
+        this.valorPagamento = valorPagamento;
+        this.metodoPagamento = metodoPagamento;
+        this.statusPagamento = statusPagamento;
+        this.idMatricula = idMatricula;
+    }
+
+    public void registrarStatus(boolean status) {
+        this.statusPagamento = status;
+    }
+
+    public boolean verificarStatus() {
+        return this.statusPagamento;
+    }
+
+    public boolean atualizarStatus(boolean novoStatus) {
+        if (this.statusPagamento == novoStatus) {
+            return false;
+        }
+        this.statusPagamento = novoStatus;
+        return true;
+    }
 }
