4e44acd724abba8292f0d0cae58db12eeb06afad
Luilu-star <luiotv2302@gmail.com>
2025-11-19 08:33:05 -0300

    add and edit classes

---
+++ src/classes/Plano.java
@@ -0,0 +1,15 @@
+package classes;
+
+public class Plano {
+    private int idPlano;
+    private String nomePlano;
+    private float duracaoPlano; 
+    private float valorPlano;
+
+    public Plano(int idPlano, String nomePlano, float duracaoPlano, float valorPlano){
+        this.idPlano = idPlano;
+        this.nomePlano = nomePlano;
+        this.duracaoPlano = duracaoPlano;
+        this.valorPlano = valorPlano;
+    }
+}
