cfda299088c6d7de776f2d173c143a61f2615dc5
Luilu-star <luiotv2302@gmail.com>
2025-12-03 08:06:57 -0300

    Reformulando esse projeto maldito

---
+++ src/main/java/classes/Aluno.java
@@ -0,0 +1,80 @@
+package classes;
+
+import java.util.Date;
+
+// Classe filha
+public class Aluno extends Usuario {
+
+    // Atributos
+    private int idAluno;
+    private String cpf;
+    private Date dataNascimento;
+    private Matricula matricula;
+
+    // Construtor
+    public Aluno (String nome, String email, String senha, int idAluno, String cpf, Date dataNascimento) {
+        
+        // Atributos herdados da classe mãe 
+        super (nome, email, senha);
+        
+        this.idAluno = idAluno;
+        this.cpf = cpf;
+        this.dataNascimento = dataNascimento;
+    }
+    
+    // Acesso às privates usando getters e setters
+    
+    // Escrever aqui!
+    
+    // Getters
+    public int getIdAluno() { return idAluno; }
+    public String getCpf() { return cpf; }
+    public Date getDataNascimento() { return dataNascimento; }
+    public Matricula getMatricula() { return matricula; }
+    
+    // Métodos abstratos implementados pela classe mãe
+    @Override
+    public void realizarLogin(String email, String senha) {
+        if (getEmail().equals(email) && getSenha().equals(senha)) {
+            setLogado(true);
+            System.out.println("Login realizado: " + getNome());
+        } else {
+            System.out.println("Login falhou");
+        }
+    }
+
+    @Override
+    public void realizarLogout() {
+        setLogado(false);
+        System.out.println("Logout: " + getNome());
+    }
+
+    @Override
+    public void cancelarMatricula() {
+        if (matricula != null) {
+            matricula.cancelarMatricula();
+            System.out.println("Matrícula cancelada: " + getNome());
+        }
+    }
+
+    // Métodos específicos da classe Aluno
+    public void realizarMatricula (Plano plano) {
+        this.matricula = new Matricula(this, plano); // O que isso significa?
+        matricula.realizarMatricula();
+    }
+    
+    public void escolherPlano (Plano plano) {
+        System.out.println("Plano escolhido: " + plano.getNomePlano());
+    }
+    
+    public void realizarPagamento (double valor, String metodo) {
+        if (matricula != null) {
+            matricula.realizarPagamento(valor, metodo);
+        }
+        
+        else {
+            System.out.println("Matrícula não encontrada!");
+        }
+    }
+
+}
